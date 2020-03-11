055c1a7b7f34473e480984792f695edff6f14faf
AgusBA011 <agustinblanco01@gmail.com>
2020-03-10 20:36:21 -0600

    Progreso dia 3/10

--- [Proyecto1]-Analisis-de-Algoritmos/Assets/LectorDeMatrices.cs
+++ [Proyecto1]-Analisis-de-Algoritmos/Assets/LectorDeMatrices.cs
@@ -7,32 +7,186 @@ public class LectorDeMatrices : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    public List<List<string>> pistasFilas = new List<List<string>>();
+
+    public  List<List<string>> pistasColumnas = new List<List<string>>();
+
+    public int rows, columns;
+    
+    public int[,] Matriz;
+
+    public Sprite sprite;
+
     void Start()
     {
+        obtainData();
+        Matriz = new int[columns, rows];
+
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < rows; j++) {
+                Matriz[i, j] = 0;
+                SpawnTile(i, -j, Matriz[i, j]);
+                
+            }
+        }
+
+
+
+    }
+
+    //Función que resuelve el nonogram utilizando backtracking
+    bool resolverNono(int [,] Matriz) {
+
+        //Revisa si quedan espacios sin marcar ya ea un color o una X
+        if (emptySpace(Matriz) == false) {
+            return true;
+        }
+
+        //Inicio del ciclo que irá celda por celda de la matriz comprobando si sive o no
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < columns; j++){
+                if (isSafe(Matriz, i, j) == true) {  //Hace un checkeo si es posible poner color en la celda dada
+
+                    Matriz[i, j] = 1;
+                    if (resolverNono(Matriz) == true) 
+                    //Llamada recursiva, hecho el cambio anterior avanza suponiendo que sí sirve. Se devuelve si no sirve.
+                    {
+                        return true;
+                    }
+                    //Si no sirve la única otra opción es que no sea ponerle color
+                    else {
+                        Matriz[i, j] = -1;
+                    }
+                }
+                else { //Si el isSafe da falso significa que es imposible poner colo en esa celda, por ende va con una X
+                    Matriz[i, j] = -1;
+                }
+            }
+        }
+        return false;
+
+    }
+    //Función que revisa si es posible colorear esa celda o no
+    bool isSafe(int [,] Matriz, int i, int j){
+
+        if (isSafeR(Matriz, i, j) == true && isSafeC(Matriz, i, j) == true) return true;
+        return false;
+
+    }
+
+
+    bool isSafeR(int[,] Matriz, int i, int j) {
+
+        //Checkeo de la fila
+        int counter = 0;
+        int slot = 0;
+        if (j == 0) return true;
+        foreach (string pista in pistasFilas[i])
+        {
+
+            while (slot != j)
+            {
+
+                if (Matriz[i, slot] == 1) counter++;
+
+                if (Matriz[i, slot] == -1) counter = 0;
+
+                if (int.Parse(pista) == counter)
+                {
+                    if (slot + 1 == j) return false;
+                    slot++;
+                    break;
+                }
+                slot++;
+
+                if (slot == j) return true;
+            }
+        }
+        return false;
+    }
+
+    bool isSafeC(int[,] Matriz, int i, int j)
+    {
+
+        //Checkeo de la fila
+        int counter = 0;
+        int slot = 0;
+        if (i == 0) return true;
+        foreach (string pista in pistasColumnas[i])
+        {
+
+            while (slot != i)
+            {
+
+                if (Matriz[slot, j] == 1) counter++;
+
+                if (Matriz[slot, j] == -1) counter = 0;
+
+                if (int.Parse(pista) == counter)
+                {
+                    if (slot + 1 == i) return false;
+                    slot++;
+                    break;
+                }
+                slot++;
+
+                if (slot == i) return true;
+            }
+        }
+        return false;
+    }
+
+
+    //Función que revisa todas las celdas y ve si falta una por colorear o poner una X
+    bool emptySpace(int[,] Matriz) {
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if ( Matriz[i,j] == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void SpawnTile(int x, int y, int value) {
+        GameObject g = new GameObject("X: " + x + "Y:" + -y);
+        g.transform.position = new Vector3(x - 2.5f, y - 2.5f);
+        var s = g.AddComponent<SpriteRenderer>();
+        s.sprite = sprite;
+
+     }
+
+
+    void obtainData() {
+
         StreamReader sr = new StreamReader("C:/Users/Agus/Documents/GitHub/Proyecto1_Analisis_de_Algoritmos/[Proyecto1]-Analisis-de-Algoritmos/Assets/nonogram.txt");
         string line = "";
 
-        string columnas, filas;
+
         string[] aux;
         short con = 0;
 
-        List<List<string>> pistasFilas = new List<List<string>>();
 
-        List<List<string>> pistasColumnas = new List<List<string>>();
 
-        while ((line = sr.ReadLine()) != null) { //Lee todas las lineas dentro del .txt
+        while ((line = sr.ReadLine()) != null)
+        { //Lee todas las lineas dentro del .txt
 
-            if (con == 0) { //Obtener el num de columnas y filas
+            if (con == 0)
+            { //Obtener el num de columnas y filas
                 aux = line.Split(',');
-                for (int i = 0; i < 2; i++) {
-                    if (i == 0) filas = aux[i];
-                    if (i == 1) columnas = aux[i];
+                for (int i = 0; i < 2; i++)
+                {
+                    if (i == 0) rows = int.Parse(aux[i]);
+                    if (i == 1) columns = int.Parse(aux[i]);
                     Debug.Log(aux[i]);
                 }
                 con++;
             }
 
-            if (line.Equals("FILAS") == true) { //Sección del txt donde dan las pistas de las filas
+            if (line.Equals("FILAS") == true)
+            { //Sección del txt donde dan las pistas de las filas
                 con++;
             }
             if (line.Equals("COLUMNAS") == true)//Sección del txt donde dan las pistas de las columnas
@@ -40,7 +194,8 @@ public class LectorDeMatrices : MonoBehaviour
                 con++;
             }
 
-            if (con == 2) { //Obtener las pistas que hay en cada fila
+            if (con == 2)
+            { //Obtener las pistas que hay en cada fila
                 if (line.Equals("FILAS") != true)
                 {
 
@@ -74,12 +229,13 @@ public class LectorDeMatrices : MonoBehaviour
 
         }
         //TESTING
-        for(int e = 0; e < pistasFilas.Count; e++)
+        for (int e = 0; e < pistasFilas.Count; e++)
         {
-            for (int i = 0; i < pistasFilas[e].Count; i++) {
+            for (int i = 0; i < pistasFilas[e].Count; i++)
+            {
 
                 Debug.Log("Pistas de filas:" + pistasFilas[e][i]);
-            }          
+            }
         }
         for (int e = 0; e < pistasColumnas.Count; e++)
         {
